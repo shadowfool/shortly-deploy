32384db7afe7127664390aca10cf4bfc87ffe137
Hack Reactor Students <accounts@hackreactor.com>
2016-05-19 17:23:57 -0700

    Cleaned up Directories

--- app/config.js
+++ app/config.js
@@ -44,71 +44,7 @@ db.once('open', function() {
   db.User = mongoose.model('User', userSchema);
   db.Link = mongoose.model('Link', linkSchema);
 
-
-  // var testUser = new User({username: 'super1', password: 'fluffy'});
-  // testUser.hashPassword();
-  // testUser.save().then(user => {
-  //   User.find({username: 'super1'}).then(user => {
-  //     user[0].comparePassword('fluffy', isMatch => console.log('is it a match?', isMatch));
-  //   });
-  // });
-
-
 });
 
 module.exports = db;
 
-  // var testUser = new User({username: 'hello', password: 'goodbye'});
-  // testUser.save(function(err, user) {
-  //   if (err) {
-  //     return console.error(err);
-  //   }
-  //   User.find(function(err, user) {
-  //     if (err) {
-  //       return console.error(err);
-  //     }
-  //     console.log(user);
-  //   });
-  // });
-
-
-// var path = require('path');
-// var knex = require('knex')({
-//   client: 'sqlite3',
-//   connection: {
-//     filename: path.join(__dirname, '../db/shortly.sqlite')
-//   },
-//   useNullAsDefault: true
-// });
-// var db = require('bookshelf')(knex);
-
-// db.knex.schema.hasTable('urls').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('urls', function (link) {
-//       link.increments('id').primary();
-//       link.string('url', 255);
-//       link.string('baseUrl', 255);
-//       link.string('code', 100);
-//       link.string('title', 255);
-//       link.integer('visits');
-//       link.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
-
-// db.knex.schema.hasTable('users').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('users', function (user) {
-//       user.increments('id').primary();
-//       user.string('username', 100).unique();
-//       user.string('password', 100);
-//       user.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
-
-// module.exports = db;
